a243499145bc0f78468944ae1020ae4223c574f4
cmichel <cmichel@interparking.com>
2021-03-13 17:00:21 +0100

    Add minimum to use entity framework

---
+++ MinimumToStart/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimumToStart.Data.Entities;
+
+namespace MinimumToStart.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MinimumToStart");
+        }
+    }
+}
---
+++ MinimumToStart/Program.cs
@@ -0,0 +1,32 @@
+﻿using MinimumToStart.Data;
+using MinimumToStart.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimumToStart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var context = new ApplicationDbContext();
+            await context.Database.EnsureCreatedAsync();
+
+            var person = new Person
+            {
+                Name = "Cédric Michel"
+            };
+
+            await context.Persons.AddAsync(person);
+            await context.SaveChangesAsync();
+
+            var dataBasePerson = context.Persons.FirstOrDefault();
+
+            Console.WriteLine(dataBasePerson.Name);
+
+        }
+    }
+}
