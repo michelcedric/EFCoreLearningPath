0af4abe3bf829513536a3771498d32a8409be368
cmichel <cmichel@interparking.com>
2021-03-13 17:17:41 +0100

    hardcoded configuration

---
+++ Configuration/Configuration/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimumToStart.Data.Entities;
+
+namespace MinimumToStart.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Configuration/Configuration/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimumToStart.Data;
+using MinimumToStart.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimumToStart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var applicationOptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            applicationOptionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MinimumToStart");
+
+
+            var context = new ApplicationDbContext(applicationOptionsBuilder.Options);
+            await context.Database.EnsureCreatedAsync();
+
+            var person = new Person
+            {
+                Name = "Cédric Michel"
+            };
+
+            await context.Persons.AddAsync(person);
+            await context.SaveChangesAsync();
+
+            var dataBasePerson = context.Persons.FirstOrDefault();
+
+            Console.WriteLine(dataBasePerson.Name);
+
+        }
+    }
+}
